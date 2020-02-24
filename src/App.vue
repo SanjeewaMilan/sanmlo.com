35359ff94020c7523a5ab74105b083bb47173586
Sanjeewa Milan <sanjeewamj@gmail.com>
2020-02-24 23:40:36 +0530

    start vue application

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class='row'>
+      <div class='col-sm-4'>
+        <SideBar></SideBar>
+      </div><!--/.col-md-4-->
+      <div class='col-sm-8'></div><!--/.col-md-8-->
+    </div><!--/.row-->
+    
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="SideBar">
+    <div class="logo">
+      <img src="../assets/san-mlo-logo.png" alt="" width='45%'>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.SideBar{
+  background:#243B55;
+  color:white;
+  height:98vh;
+}
+</style>
