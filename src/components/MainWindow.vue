b39c175bfe2d19cf0fb35b47191633027f0fdee8
Sanjeewa Milan <sanjeewamj@gmail.com>
2020-02-25 22:38:14 +0530

    main layout & colors completed

---
+++ src/components/MainWindow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="MainWindow">
+    <div>
+      aha
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainWindow',
+  props: {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.MainWindow {
+    background:#243B55;
+    height: 98vh;
+    background-image: radial-gradient( #243B55,#141E30);
+}
+</style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,6 +3,14 @@
     <div class="logo">
       <img src="../assets/san-mlo-logo.png" alt="" width='45%'>
     </div>
+    <div>
+      <b-nav vertical class="w-25">
+        <b-nav-item >Active</b-nav-item>
+        <b-nav-item>Link</b-nav-item>
+        <b-nav-item>Another Link</b-nav-item>
+        <b-nav-item >Disabled</b-nav-item>
+      </b-nav>
+    </div>
   </div>
 </template>
 
@@ -18,8 +26,14 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .SideBar{
-  background:#243B55;
+  background:#141E30;
   color:white;
   height:98vh;
+  border-color: white;
+  border-right-style: solid;
+   border-width:1px;
+}
+.nav-item a{
+  color:white;
 }
 </style>
